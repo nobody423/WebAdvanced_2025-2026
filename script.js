6db13b2697ec71f490c676970ff6c8fa17536d9a
nobody423 <sophielaureys7@gmail.com>
2026-05-10 12:12:51 +0200

    Update index.html with Pokemon API list options

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+const all = document.getElementById("all");
+
+
 const BASE_URL = "https://pokeapi.co/api/v2/";
 
-async function getAll() {
-    const res = await fetch(BASE_URL);
 
-    if(!res.ok) {
-        throw new Error(`Pokemon not found: ${name}`);
+async function fetchPokemon() {
+
+    try {
+        const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+        if(!response.ok) {
+            throw new Error("could not fetch data");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+    } 
+    catch (error) {
+        console.log(error)
     }
-
-    const data = res.json();
-
-
-
 }
-
